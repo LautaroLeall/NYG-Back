882e5d446edf80966c2daf30231bc78a3d364c26
LautaroLeall <lautaroleal4@gmail.com>
2026-09-14 21:03:15 -0300

    feat: endpoints de alertas disciplinarias y cálculo automático de estadísticas

--- src/routes/statsRoutes.js
+++ src/routes/statsRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { protect, admin, authorize } = require('../middlewares/authMiddleware');
 
 const {
-  getRankings
+  getRankings,
+  getAlerts
 } = require('../controllers/statsController');
 
 // Endpoint de rankings
 // GET /api/stats/rankings?tipo=goleadores
 router.route('/rankings')
   .get(getRankings);
+
+router.route('/alerts')
+  .get(protect, admin, getAlerts);
 
 module.exports = router;
